8d257ec3aab19e441a9ac2d38b88c1e9405bea61
smb2796 <sean.brown2796@gmail.com>
2021-03-03 21:38:34 -0500

    Add in Balancer

--- governance/GetGovernanceParticipants.js
+++ governance/GetGovernanceParticipants.js
@@ -1,12 +1,9 @@
 const { default: axios } = require("axios");
 const fs = require("fs");
 
-// This address list was collected from https://beta.mcdgov.info
-const MakerAddresses = require("./Maker_Governance.json");
-
 // Edit this URL to query different Snapshot DAOs
 const url =
-  "https://hub.snapshot.page/api/voters?to=1612976449&spaces=yam.eth,Sushi,badgerdao.eth,yearn";
+  "https://hub.snapshot.page/api/voters?to=1612976449&spaces=yam.eth,Sushi,badgerdao.eth,yearn,Balancer";
 
 // Gathers addresses from snapshot URL
 async function getGovernanceParticipants() {
@@ -22,20 +19,11 @@ async function getGovernanceParticipants() {
     governanceParticipants[voter] = 80;
   });
 
-  MakerAddresses.forEach((voter) => {
-    if (voter.Since < "2021-02-10") {
-      voter = voter.Address;
-      voter = voter.toLowerCase();
-
-      governanceParticipants[voter] = 80;
-    }
-  });
-
   governanceParticipants = JSON.stringify(governanceParticipants, null, 2);
 
   // Writes to a json file in ./outputs
   fs.writeFileSync('./outputs/governance_recipients.json', governanceParticipants);
-  console.log("Successfully gathered governance participant addresses for Maker, Yam, Sushi, BadgerDAO and Yearn");
+  console.log("Successfully gathered governance participant addresses for Balancer, Yam, Sushi, BadgerDAO and Yearn");
 }
 
 module.exports = {
